d3017d28198f337a1ceb60ecbd39184e537aed17
toshiki <tosiki219@stu.kanazawa-u.ac.jp>
2023-08-16 14:57:05 +0900

    Added win-loss logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import "./App.css";
 import Board from "./components/Board";
 
 function App() {
-  const [board, setBoard] = useState<Array<Array<string | null>>>([
+  type CellValue = "X" | "O" | null;
+
+  const [board, setBoard] = useState<CellValue[][]>([
     [null, null, null],
     [null, null, null],
     [null, null, null],
   ]);
 
   const [currentPlayer, setCurrentPlayer] = useState<"X" | "O">("X");
-  const [winner, setWinner] = useState<string | null>(null);
+  const [winner, setWinner] = useState<CellValue | "Draw">(null);
 
   const handleCellClick = (row: number, col: number) => {
     // 既にセルが埋まっている、またはゲームが終了している場合は何もしない
@@ -25,22 +27,78 @@ function App() {
     setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
   };
 
+  const resetGame = () => {
+    setBoard([
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ]);
+    setCurrentPlayer("X");
+    setWinner(null);
+  };
+
   useEffect(() => {
-    // 勝利条件のチェックなど、ゲームの結果を計算するロジックをここに実装
-    // 例: 3つ並んだら勝者をsetWinnerでセットする
-
-    // 簡易的な勝者の判定
-    for (let i = 0; i < 3; i++) {
-      if (
-        board[i][0] &&
-        board[i][0] === board[i][1] &&
-        board[i][1] === board[i][2]
-      ) {
-        setWinner(board[i][0]);
+    const checkWinner = (board: CellValue[][]): CellValue | "Draw" => {
+      // 横、縦、斜めの勝利条件をチェック
+      for (let i = 0; i < 3; i++) {
+        // 横の勝利条件
+        if (
+          board[i][0] &&
+          board[i][0] === board[i][1] &&
+          board[i][0] === board[i][2]
+        ) {
+          return board[i][0];
+        }
+        // 縦の勝利条件
+        if (
+          board[0][i] &&
+          board[0][i] === board[1][i] &&
+          board[0][i] === board[2][i]
+        ) {
+          return board[0][i];
+        }
       }
-    }
+      // 左上から右下への斜めの勝利条件
+      if (
+        board[0][0] &&
+        board[0][0] === board[1][1] &&
+        board[0][0] === board[2][2]
+      ) {
+        return board[0][0];
+      }
+      // 右上から左下への斜めの勝利条件
+      if (
+        board[0][2] &&
+        board[0][2] === board[1][1] &&
+        board[0][2] === board[2][0]
+      ) {
+        return board[0][2];
+      }
 
-    // ... 他にも横、斜めの勝者判定を追加
+      // 引き分けの条件
+      if (isBoardFull(board)) {
+        return "Draw";
+      }
+
+      // 勝利者がいない場合はnullを返す
+      return null;
+    };
+
+    const isBoardFull = (board: CellValue[][]): boolean => {
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          if (!board[i][j]) {
+            return false; // セルが空の場合、ボードはまだ埋まっていない
+          }
+        }
+      }
+      return true; // 全てのセルが埋まっている
+    };
+
+    const result = checkWinner(board);
+    if (result === "X" || result === "O" || result === "Draw") {
+      setWinner(result);
+    }
   }, [board]);
 
   return (
